4218248e66ebda104a1948bb98568d581e89606b
Isaac Pereira Da Silva <isaacsilva15663@gmail.com>
2025-04-18 00:10:51 -0300

    first commit

---
+++ src/exercicios/now.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class now {
+
+	public static void main(String[] args) {
+		
+	/*LocalDateTime agora = LocalDateTime.now();
+		System.out.println("Data e hora atual: " + agora);
+		*/
+		
+		
+		LocalDateTime fivedays = LocalDateTime.now().plusDays(5);
+		System.out.println(fivedays);
+	}
+}
