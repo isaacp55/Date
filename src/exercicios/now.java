9c6c48b2a8e0a2027badeecc9602a2da99924036
Isaac Pereira Da Silva <isaacsilva15663@gmail.com>
2025-04-19 06:01:00 -0300

    exibindo dia, mes, ano, horario e minuto

--- src/exercicios/now.java
+++ src/exercicios/now.java
@@ -1,6 +1,7 @@
 package exercicios;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class now {
 
@@ -20,11 +21,30 @@ public class now {
 		
 	//SimpleDateFormat sdf = new SimpleDateFormat();
 	
-	LocalDateTime december = LocalDateTime.of(2025, 12, 25, 20, 30);
-	
-	System.out.println(december.getChronology());
-	}
-
-	
-	
+	/*LocalDateTime december = LocalDateTime.of(2025, 12, 25, 20, 30);	
+	System.out.println(december);
+	*/
+		
+		/* // Data e hora atual
+        LocalDateTime agora = LocalDateTime.now();
+        // Data futura (exemplo: 1º de janeiro de 2030 às 10h)
+        LocalDateTime futura = LocalDateTime.of(2030, 1, 1, 10, 0);
+        // Verifica se a data futura é depois da atual
+        boolean isDepois = futura.isAfter(agora);
+        // Exibe o resultado
+        System.out.println("A data futura é depois da atual? " + isDepois);
+	    */
+		
+		/*LocalDateTime agora = LocalDateTime.now();	
+		DateTimeFormatter dias = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String formatado = agora.format(dias);
+		System.out.println(formatado);
+		*/
+		
+		/*LocalDateTime now = LocalDateTime.now();	
+		DateTimeFormatter days = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		String formated = now.format(days);
+		System.out.println(formated);
+		*/
+	}	
 }
